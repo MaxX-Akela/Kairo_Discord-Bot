8873b8fe1e7499d32d4106425877620a2dd4e932
MaxX_Akela <145983198+MaxX-Akela@users.noreply.github.com>
2024-08-09 11:01:10 +0700

    User info/Информация о пользователе
    ID пользователя, Имя пользователя, Аккаунт создан, Присоединился к серверу, Нахождение в голосовом канале.
    User ID, Username, Account created, Joined server, In voice channel.

---
+++ cogs/userinfo.py
@@ -0,0 +1,33 @@
+import disnake
+from disnake.ext import commands
+
+class UserInfo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(description="Информация о пользователе на сервере")
+    async def userinfo(self, ctx, member: disnake.Member = None):
+        if member is None:
+            member = ctx.author  # Если пользователь не указан, используем автора сообщения
+
+        embed = disnake.Embed(title=f"Информация о пользователе: {member.name}", color=disnake.Color.blue())
+        embed.add_field(name="ID пользователя", value=member.id, inline=False)
+        embed.add_field(name="Имя пользователя", value=member.mention, inline=False)
+        embed.add_field(name="Аккаунт создан", value=member.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
+        embed.add_field(name="Присоединился к серверу", value=member.joined_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
+        
+        # Добавляем информацию о статусе и голосовой активности
+        
+        
+        if member.voice:
+            embed.add_field(name="В голосовом канале?", value=f"В канале: {member.voice.channel.name}", inline=False)
+        else:
+            embed.add_field(name="В голосовом канале?", value="Не в голосовом канале", inline=False)
+
+        embed.set_thumbnail(url=member.avatar.url if member.avatar else None)
+
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(UserInfo(bot))
+
