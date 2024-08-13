1e4b169dd6c88fc994d76329886d90419ad3af4e
MaxX_Akela <145983198+MaxX-Akela@users.noreply.github.com>
2024-08-13 14:15:57 +0700

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -9,7 +9,7 @@ class PingCommand(commands.Cog):
     @commands.slash_command(description="Узнай задержку бота")
     async def ping(self, inter: disnake.ApplicationCommandInteraction):
         latency = round(self.bot.latency * 1000)
-        embed = disnake.Embed(title="Понг!", description=f"Задержка: {latency} мс", color=disnake.Color.blue())
+        embed = disnake.Embed(title="Понг!", description=f"Задержка: {latency} мс", color=0x0000FF)
         await inter.response.send_message(embed=embed)
 
 def setup(bot: commands.Bot):
